bf1387a9e96890d843cfe9a545a686d94109b3d1
Yifan Zhou <yifan.zhou11882-bigz@basischina.com>
2024-02-12 01:06:46 +0800

    Update Task 3.py

--- QMLS Code/Task 3.py
+++ QMLS Code/Task 3.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.optim as optim
 import numpy as np
 import esm # pre-trained ESM2 model
-
 # Define encode functions
 def encode(molecule):
     # molecule is a numpy array of shape (N, 4), where N is the number of amino acids, and 4 corresponds to x, y, z, and type
