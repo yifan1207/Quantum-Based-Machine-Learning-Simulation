2b355ea851a4ae52bace87b7294b8db5fcb92df6
Yifan Zhou <yifan.zhou11882-bigz@basischina.com>
2024-06-11 10:55:54 +0800

    Update Task 1.py

--- QMLS Code/Task 1.py
+++ QMLS Code/Task 1.py
@@ -1,6 +1,6 @@
 import torch   
 import torch.nn as nn    
-import torch.optim as optim
+import torch.optim as optim 
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
